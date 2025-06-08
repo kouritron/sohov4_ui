1df1263cc463a7b946816e438cd813fbe8ea3f5c
kazra <204182586+kazra2025@users.noreply.github.com>
2025-06-08 11:06:49 -0400

    styles working now

--- src_ui/index.jsx
+++ src_ui/index.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 // import App from './App';
 import C2FTable from './components/C2FTable';
+import './styles/GlobalStyles.css';
+
 
 console.log("index.js: About to create root")
 const root = createRoot(document.getElementById('sohov4_root'));
