01052ce53b2847392b8b41b9b4e05dc3804d3d20
kazra <204182586+kazra2025@users.noreply.github.com>
2025-06-09 00:43:17 -0400

    s4 grid improved

--- src_ui/components/C2FTableGrid.jsx
+++ src_ui/components/C2FTableGrid.jsx
@@ -11,16 +11,16 @@ const C2FTableGrid = () => {
         <div className="s4_grid_small s4_border7_dark s4_mono_text s4_centered_text">
             <div className="s4_sep_line14 s4_row" />
             <div className="s4_row s4_center_justified">
-                <div className="col_10_20  s4_text_large s4_text_bold ">Celsius</div>
-                <div className="col_10_20  s4_text_large s4_text_bold ">Fahrenheit</div>
+                <div className="col_50  s4_text_large s4_text_bold ">Celsius</div>
+                <div className="col_50  s4_text_large s4_text_bold ">Fahrenheit</div>
              </div>
             <div className="s4_sep_line14 s4_row" />
 
             {celsius_values.map((c, i) => (
                 <React.Fragment key={c}>
                     <div className="s4_row">
-                        <div className="col_10_20 s4_padded_10 s4_text_medium ">{c}</div>
-                        <div className="col_10_20 s4_padded_10 s4_text_medium ">{celsius2fahrenheit(c)}</div>
+                        <div className="col_50 s4_padded_10 s4_text_medium ">{c}</div>
+                        <div className="col_50 s4_padded_10 s4_text_medium ">{celsius2fahrenheit(c)}</div>
                     </div>
                     {i !== (celsius_values.length - 1) && (
                         <div className="s4_sep_line7 s4_row" />
